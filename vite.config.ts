ac107a37c97c24e6724ca25c42dcbb319716dff5
William <williambosworth420@gmail.com>
2026-01-18 00:12:25 +0100

    william: the Commit+

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
       "gwfrontend.onrender.com",
       ".onrender.com", // Allow all Render subdomains
       "localhost",
+      "genius.ng",
     ],
     proxy: {
       "/api": {
